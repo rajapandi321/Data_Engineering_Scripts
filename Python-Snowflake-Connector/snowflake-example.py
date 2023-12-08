6712734069571c3574c2e9a86479695b662be65a
Raja Pandi GR <rajapandi.gr@optisolbusiness.com>
2023-12-08 15:09:05 +0530

    New File

---
+++ Python-Snowflake-Connector/snowflake-example.py
@@ -0,0 +1,30 @@
+import snowflake.connector
+
+# Establishing connection parameters
+conn = snowflake.connector.connect(
+    user='JEEVAN',
+    password='Optisol@2023',
+    account='oozelmy-lxb06506',
+    warehouse='COMPUTE_WH',
+    database='SNOWFLAKE_PYTHON',
+    schema='PUBLIC',
+    role='ACCOUNTADMIN'
+)
+
+# Creating a cursor object using the connection
+cur = conn.cursor()
+
+# Set the schema and database context
+cur.execute("USE DATABASE SNOWFLAKE_PYTHON")
+cur.execute("USE SCHEMA PUBLIC")
+
+# Execute your query
+cur.execute('create table student(id int,name varchar(20),mark int)')
+rows = cur.fetchall()
+
+for row in rows:
+    print(row)
+
+# Closing the cursor and connection
+cur.close()
+conn.close()
